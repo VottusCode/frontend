809fcbbe2281b1a0860f4ef6b406c222b9d4e35b
VottusCode <vottus@vott.us>
2020-10-18 20:37:59 +0200

    deprecation note

--- src/context/UserContext.ts
+++ src/context/UserContext.ts
@@ -5,4 +5,5 @@ const defaultUser = { authenticated: false, user: null };
 const UserContext = createContext(defaultUser);
 UserContext.displayName = "UserContext";
 
+/** @deprecated Using Apollo's cached me result */
 export default UserContext;
